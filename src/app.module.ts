be5373790bd67809b994a82253f36405c981d603
Lautaro Arizmendi <lautaroarizmendi@gmail.com>
2022-11-19 17:05:09 -0300

    Deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { PlayersModule } from './players/players.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Player } from './players/entities/player.entity';
 import { Team } from './teams/entities/team.entity';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
@@ -13,11 +15,11 @@ import { Team } from './teams/entities/team.entity';
     TeamsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'fulbo',
-      password: 'del0al9',
-      database: 'db_fulbo',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Player, Team],
       synchronize: true,
     }),
