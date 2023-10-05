bf3b86795b1dc0e589d5d74bd6bbcc002a8a53dd
Lautaro Arizmendi <lautaroarizmendi@gmail.com>
2023-10-05 20:44:17 -0300

    Team's Test

--- src/teams/teams.controller.spec.ts
+++ src/teams/teams.controller.spec.ts
@@ -1,34 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TeamsController } from './teams.controller';
 import { TeamsService } from './teams.service';
-import { Player } from 'src/players/entities/player.entity';
-import { Team } from './entities/team.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 describe('TeamsController', () => {
   let controller: TeamsController;
+  const mock = {
+    teams: [
+      {
+        id: 1,
+        name: 'River Plate',
+        stadium: 'Mas Monumental',
+        badges: 'CARP',
+        year_fundation: '1901',
+      },
+      {
+        id: 2,
+        name: 'Boca Juniors',
+        stadium: 'La Bombonera',
+        badges: 'CABJ',
+        year_fundation: '1905',
+      },
+    ],
+    findAll() {
+      return this.teams;
+    },
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TeamsController],
       providers: [TeamsService],
-      imports: [
-        TypeOrmModule.forRoot({
-          type: 'postgres',
-          host: process.env.DB_HOST,
-          port: parseInt(process.env.DB_PORT),
-          username: process.env.DB_USER,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_NAME,
-          entities: [Player, Team],
-          synchronize: true,
-        }),
-        TypeOrmModule.forFeature([Team]),
-      ],
-    }).compile();
-
+    })
+      .overrideProvider(TeamsService)
+      .useValue(mock)
+      .compile();
     controller = module.get<TeamsController>(TeamsController);
   });
 
@@ -36,14 +41,8 @@ describe('TeamsController', () => {
     expect(controller).toBeDefined();
   });
 
-  // it('findAllTeams => should return an array of Teams', async () => {
-  //   const team = {
-  //     name: 'River Plate',
-  //     stadium: 'Más Monumental',
-  //     badges: 'CARP',
-  //     year_fundation: '1901',
-  //   };
-  //   const result = await controller.findAllTeams();
-  //   expect(result).toEqual(team);
-  // });
+  it('findAllTeams => should return an array of Teams', async () => {
+    const result = await controller.findAllTeams();
+    expect(result).toEqual(mock.teams);
+  });
 });
